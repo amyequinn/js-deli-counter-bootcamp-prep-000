1a9faf0d84aa507df2836c926e798b9024bde83b
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-03 14:17:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-var katzDeli = [];
 
-function takeANumber(line, name){
-  var currentPlace = line.length +1;
-  var welcome = "Welcome, " + name+". You are number " + currentPlace + " in line.";
-  line.push(name);
-return welcome;
-
-}
-
-
-function nowServing(line){
-var outputString;
-if (currentLine.length === 0){
-outputString = “There is nobody waiting to be served!”
-}
-else{
-outputString = “Currently serving ” + currentLine[0] + “.”;
-currentLine.shift();
-}
-return outputString;
-}
-
-function currentLine(line){
-var outputString;
-if (line.length === 0){
-outputString = “The line is currently empty.”
-}
-else{
-outputString = “The line is currently: ”
-for(var i = 0; i < line.length ; i++){
-if (i === 0){
-
-outputString = outputString + (i+1) + “. ” + line[i];
-}
-else{
-outputString = outputString + “, ” + (i+1) + “. ” + line[i];
-}
-}
-}
-return outputString;
+function takeANumber(katzDeliLine, name){
+  return ("Welcome, " +name+ "You are number" +katzDeliLine+ + "in line".);
 }
