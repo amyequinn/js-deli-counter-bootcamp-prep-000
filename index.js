91909ba43e89bfae4b51fe5fe65e75493f75b8ad
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-03 15:28:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,15 +16,16 @@ return "There is nobody waiting to be served!";
   return currentlyServing;
 }
 
+
+
 function currentLine(katzDeliLine){
   if (katzDeliLine.length===0){
   return "The line is currently empty.";
   }
 
-  var line = `The line is currently: 1. ${katzDeliLine[0]}`;
-
- for(let i = 1; i<katzDeliLine.length;i++){
- line += `, ${i+1}. ${katzDeliLine[i]}`
-}
-return line
-}
+     var customerAndNumber = [];
+     for (var i=0; i<katzDeliLine.length; i++) {
+     customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
+  }
+    return "The line is currently: " + customerAndNumber.join(", ");
+  }
