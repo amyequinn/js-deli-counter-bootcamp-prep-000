4c178bf5cfb808464c86feb02a1475495d4594f6
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-03 14:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 function takeANumber(katzDeliLine, name){
-  var welcome = ("Welcome, " +name+ "You are number" +katzDeliLine+ + "in line".);
+  var welcome = ("Welcome, " +name+ "You are number" +katzDeliLine+ "in line".);
   return welcome;
 }
