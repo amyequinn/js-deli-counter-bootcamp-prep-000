50b8309b66ea999d82e2c96152eae9fce7463ba2
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-03 14:43:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
 
-if katzDeliLine===0;
+if (katzDeliLine===0);
 return "There is nobody waiting to be served!";}
 
 else {
