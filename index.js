ca95a290e48d55d0a16a6d51dbd8b983808b0399
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-04-03 15:24:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,17 +21,10 @@ function currentLine(katzDeliLine){
   return "The line is currently empty.";
   }
 
-  var myString = `The line is currently: 1. ${katzDeliLine[0]}`
+  var line = `The line is currently: 1. ${katzDeliLine[0]}`;
 
  for(let i = 1; i<katzDeliLine.length;i++){
- myString += `, ${i+1}. ${katzDeliLine[i]}`
+ line += `, ${i+1}. ${katzDeliLine[i]}`
 }
-return myString
-}
-
-/*
-var number = katzDeliLine.length;
-var name = katzDeliLine.unshift();
-var line = ("The line is currently: " +number+ ". " +name+);
-  return line;
+return line
 }
